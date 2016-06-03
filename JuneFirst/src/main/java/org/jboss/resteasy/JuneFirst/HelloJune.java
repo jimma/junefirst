1142e4ccd7702c9b6bb2ae698d37df583343ea41
TengYan <18910269968@163.com>
2016-06-03 10:39:02 +0800

    including codes

---
+++ JuneFirst/src/main/java/org/jboss/resteasy/JuneFirst/HelloJune.java
@@ -0,0 +1,45 @@
+package org.jboss.resteasy.JuneFirst;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloJune extends HttpServlet{
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = -3014434748135025399L;
+
+	@Override
+	public void init() throws ServletException{
+		super.init();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException,IOException{
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Hello June !!\n JuneFirst!! </h1>");
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException,IOException{
+	}
+	
+	@Override
+	public void destroy(){
+		super.destroy();
+	}
+
+
+}
